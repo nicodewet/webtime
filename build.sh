ba959697604a025701a9d4a67792b8b5c5cea25d
Nico de Wet <nico@nicodewet.com>
2016-11-11 16:14:27 +1300

    build.sh does dotnet restore, publish then docker
    later is image build

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -ev
+# dotnet restore uses NuGet to restore dependencies as well as project-specific tools that are specified in the project.json file.
+# An optional NuGet.config file specifies the feeds where the packages are located. By default packages are located
+# in ~/.nuget/packages
 dotnet restore
-dotnet build
+# dotnet publish compiles the application, reads through its dependencies specified in the project.json file and publishes the resulting
+# set of files to a directory
+dotnet publish
+cd bin/Debug/netcoreapp1.0/publish && docker build -t nicodewet/timeapp .
