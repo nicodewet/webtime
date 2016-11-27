0d1a83a07bab9c987ef67b2cd458a6e8828e47b6
Nico de Wet <nico@nicodewet.com>
2016-11-28 10:57:57 +1300

    test

--- snt-docker-deploy.sh
+++ snt-docker-deploy.sh
@@ -2,11 +2,14 @@
 ssh -i /var/lib/jenkins/.ssh/deploy-user.time.makaro.io deploy-user@time.makaro.io -p 5024 <<+
 echo "*** CHECK RUNNING CONTAINERS ***"
 docker ps
-# docker stop grave_saha && docker rm -v grave_saha
+echo "*** STOP RUNNING CONTAINER ***"
+docker stop sick_leakey && docker rm -v sick_leakey
+echo "*** CHECK RUNNING CONTAINERS ***"
+docker ps
 echo "*** PULL LATEST IMAGE ***"
 docker pull nicodewet/timeapp:0.0.1-SNAPSHOT
 echo "*** RUN LATEST IMAGE ***"
-docker run -d -p 80:5000 nicodewet/timeapp:0.0.1-SNAPSHOT --name timeapp
+docker run -d -p 80:5000 --name timeapp nicodewet/timeapp:0.0.1-SNAPSHOT
 echo "*** CHECK RUNNING CONTAINERS **"
 docker ps
 +
