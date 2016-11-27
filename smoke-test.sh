96aee68ec48b1021cd431719b90b7d7f6147e844
Nico de Wet <nico@nicodewet.com>
2016-11-28 11:04:59 +1300

    simpler test command

--- smoke-test.sh
+++ smoke-test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 http_code=$(curl --write-out %{http_code} --silent --output /dev/null time.makaro.io)
 echo "HTTP RESPONSE CODE: $http_code"
-test "$http_code" != '200' && (echo http response code was not 200; exit 1)
+echo "HTTP RESPONSE CODE SHOULD BE 200"
+test $http_code == "200"
