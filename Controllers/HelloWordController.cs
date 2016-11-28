62398fcef8d6b5c76d8ea1aeaaa184a97bf4f336
Nico de Wet <nico@nicodewet.com>
2016-11-28 21:29:58 +1300

    adding Microsoft.DotNet.Watcher.Tools and Microsoft.AspNetCore.Mvc

---
+++ Controllers/HelloWordController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NicoDeWet.WebTime
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
